6723d537cfdfafca5ef7227356544c18488f4d91
MA-REX <m.amin.rex@gmail.com>
2025-05-17 08:58:58 +0330

    Update settings.py

--- digikala/settings.py
+++ digikala/settings.py
@@ -133,9 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
     
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
